37f22cb9cf082038fe96bd83e12a925360b7dc20
elayhakim <elay.hakim@post.runi.ac.il>
2025-12-19 20:03:23 +0200

    Completed Runigram project and all editors

--- Editor4.java
+++ Editor4.java
@@ -2,18 +2,11 @@ import java.awt.Color;
 
 public class Editor4 {
     public static void main (String[] args) {
-        // קבלת שם הקובץ ומספר הצעדים משורת הפקודה
         String fileName = args[0];
         int n = Integer.parseInt(args[1]);
-        
-        // קריאת תמונת המקור
-        Color[][] sourceImage = Runigram.read(fileName);
-        
-        // יצירת תמונת היעד (גרסת גווני האפור של המקור)
-        Color[][] targetImage = Runigram.grayScaled(sourceImage);
-        
-        // הגדרת הקנבס והצגת האנימציה
-        Runigram.setCanvas(sourceImage);
-        Runigram.morph(sourceImage, targetImage, n);
+        Color[][] source = Runigram.read(fileName);
+        Color[][] grayscale = Runigram.grayScaled(source);
+        Runigram.setCanvas(source);
+        Runigram.morph(source, grayscale, n);
     }
 }
